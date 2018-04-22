b6a6be0844c91170a6c8519e8485a61d6646188e
Christiebelle <cslester@outlook.com>
2018-04-22 13:03:08 +0100

    multiply small nums test passing

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -47,4 +47,30 @@ describe('calculator', function () {
     assert.equal(12171, calculator.runningTotal);
   })
 
+  it('can multiply small nums', function(){
+    calculator.clearClick();
+    calculator.numberClick(2);
+    calculator.numberClick(0);
+    calculator.operatorClick("*");
+    calculator.numberClick(8);
+    calculator.operatorClick("=");
+    assert.equal(160, calculator.runningTotal);
+  })
+
+  // it('can multiply large nums', function(){
+  //   calculator.clearClick();
+  //   calculator.numberClick(2);
+  //   calculator.numberClick(0);
+  //   calculator.numberClick(5);
+  //   calculator.numberClick(0);
+  //   calculator.numberClick(0);
+  //   calculator.operatorClick("*");
+  //   calculator.numberClick(8);
+  //   calculator.numberClick(3);
+  //   calculator.numberClick(2);
+  //   calculator.numberClick(9);
+  //   calculator.operatorClick("=");
+  //   assert.equal(12171, calculator.runningTotal);
+  // })
+
 });
