3eafba3fe494e3f8aa6129900ad2792cf54c0b52
Christiebelle <cslester@outlook.com>
2018-04-22 14:59:03 +0100

    testing for working methods with decimals written and passing

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -162,13 +162,13 @@ describe('calculator functionality', function() {
       expect(running_total.getAttribute('value')).to.eventually.equal('621483608');
     })
 
-    // it('should have a working / operator', function(){
-    //   running_total = element(by.css('#running_total'))
-    //   element(by.css('#number8')).click();
-    //   element(by.css('#operator_divide')).click();
-    //   element(by.css('#number4')).click();
-    //   element(by.css('#operator_divide')).click();
-    //   expect(running_total.getAttribute('value')).to.eventually.equal('2');
-    // })
+    it('should work with decimals', function(){
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number8')).click();
+      element(by.css('#operator_divide')).click();
+      element(by.css('#number3')).click();
+      element(by.css('#operator_divide')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('2.6666666666666665');
+    })
 
 });
