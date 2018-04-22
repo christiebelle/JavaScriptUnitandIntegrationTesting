52234efb57e8a73187cd790cac650246f9d2a79d
Christiebelle <cslester@outlook.com>
2018-04-22 14:29:21 +0100

    test for mupltiple functions chained together written and passing

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -113,4 +113,17 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2');
   })
 
+  it('should be able to chain events together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_subtract')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('14');
+  })
+
 });
