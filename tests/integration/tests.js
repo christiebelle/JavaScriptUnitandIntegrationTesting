ffc82e11697f2ed9da1e5ab9fe77727aa75daeff
Christiebelle <cslester@outlook.com>
2018-04-22 14:48:32 +0100

    testing for working methods with large integers

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -145,15 +145,23 @@ describe('calculator functionality', function() {
       expect(running_total.getAttribute('value')).to.eventually.equal('-2');
     })
 
-    // it('should have a working * operator', function(){
-    //   running_total = element(by.css('#running_total'))
-    //   element(by.css('#number2')).click();
-    //   element(by.css('#operator_multiply')).click();
-    //   element(by.css('#number6')).click();
-    //   element(by.css('#operator_multiply')).click();
-    //   expect(running_total.getAttribute('value')).to.eventually.equal('12');
-    // })
-    //
+    it('should work with large integers', function(){
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number2')).click();
+      element(by.css('#number3')).click();
+      element(by.css('#number7')).click();
+      element(by.css('#number9')).click();
+      element(by.css('#number3')).click();
+      element(by.css('#number4')).click();
+      element(by.css('#operator_multiply')).click();
+      element(by.css('#number2')).click();
+      element(by.css('#number6')).click();
+      element(by.css('#number1')).click();
+      element(by.css('#number2')).click();
+      element(by.css('#operator_multiply')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('621483608');
+    })
+
     // it('should have a working / operator', function(){
     //   running_total = element(by.css('#running_total'))
     //   element(by.css('#number8')).click();
