0deee61eeddf749d467f4cb86ab607458789ac22
Christiebelle <cslester@outlook.com>
2018-04-22 14:42:33 +0100

    testing for working methods with negative integers

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -136,15 +136,15 @@ describe('calculator functionality', function() {
       expect(running_total.getAttribute('value')).to.eventually.equal('13');
     })
 
-    // it('should have a working - operator', function(){
-    //   running_total = element(by.css('#running_total'))
-    //   element(by.css('#number6')).click();
-    //   element(by.css('#operator_subtract')).click();
-    //   element(by.css('#number4')).click();
-    //   element(by.css('#operator_subtract')).click();
-    //   expect(running_total.getAttribute('value')).to.eventually.equal('2');
-    // })
-    //
+    it('should work with negative integers', function(){
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number6')).click();
+      element(by.css('#operator_subtract')).click();
+      element(by.css('#number8')).click();
+      element(by.css('#operator_subtract')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('-2');
+    })
+
     // it('should have a working * operator', function(){
     //   running_total = element(by.css('#running_total'))
     //   element(by.css('#number2')).click();
