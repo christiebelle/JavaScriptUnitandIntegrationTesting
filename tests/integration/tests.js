9d501b3fd06a376ff4822eb45deaa106cfb4f519
Christiebelle <cslester@outlook.com>
2018-04-22 14:12:52 +0100

    all number buttons working correctly

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -12,8 +12,62 @@ describe('calculator functionality', function() {
   // write integration tests here in the form of "it should do something..."
   it('should have working number buttons', function(){
     running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1')
+  });
+
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  });
+
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3')
+  });
+
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number4')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  });
+
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5')
+  });
+
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number6')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('6')
+  });
+
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number7')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('7')
+  });
+
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number8')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('8')
+  });
+
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('9')
+  });
+
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number0')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0')
   });
 
   it('should have a working + operator', function(){
