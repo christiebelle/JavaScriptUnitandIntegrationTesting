ac9e804e6c31ddea9307842dd7a1e6d2446acf56
Christiebelle <cslester@outlook.com>
2018-04-22 13:10:03 +0100

    large number divide test passing

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -83,20 +83,19 @@ describe('calculator', function () {
     assert.equal(10, calculator.runningTotal);
   })
 
-  // it('can divide large nums', function(){
-  //   calculator.clearClick();
-  //   calculator.numberClick(2);
-  //   calculator.numberClick(0);
-  //   calculator.numberClick(5);
-  //   calculator.numberClick(0);
-  //   calculator.numberClick(0);
-  //   calculator.operatorClick("*");
-  //   calculator.numberClick(8);
-  //   calculator.numberClick(3);
-  //   calculator.numberClick(2);
-  //   calculator.numberClick(9);
-  //   calculator.operatorClick("=");
-  //   assert.equal(170744500, calculator.runningTotal);
-  // })
+  it('can divide large nums', function(){
+    calculator.clearClick();
+    calculator.numberClick(2);
+    calculator.numberClick(4);
+    calculator.numberClick(0);
+    calculator.numberClick(0);
+    calculator.numberClick(0);
+    calculator.operatorClick("/");
+    calculator.numberClick(8);
+    calculator.numberClick(0);
+    calculator.numberClick(0);
+    calculator.operatorClick("=");
+    assert.equal(30, calculator.runningTotal);
+  })
 
 });
